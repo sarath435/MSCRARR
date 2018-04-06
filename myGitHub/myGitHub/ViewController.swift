6ceefd11f792f35a7bd21f62418c1a49f4a92e7e
rajendra reddy <rajendra.techie@achiever-108.local>
2018-04-06 11:35:58 +0530

    print statement written by rajendra

--- myGitHub/myGitHub/ViewController.swift
+++ myGitHub/myGitHub/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hai rejendra")
+        print("hi sarath")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
